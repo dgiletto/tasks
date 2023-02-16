1c6f5b2b81ba9d886929c960e4526af870edf6bb
Dylan Giletto <dgiletto@udel.edu>
2023-02-16 12:49:03 -0500

    Task 3 Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./App.css";
+import rashford from "./Assets/rashford.jpg";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+
+console.log(rashford);
 
 function App(): JSX.Element {
     return (
@@ -13,6 +20,22 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            Unordered List:
+            <ul>
+                <li>First thing</li>
+                <li>Second thing</li>
+                <li>Last but not least</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <img src={rashford} alt="The goat Marcus Rashford" />
+            <Container>
+                <Row>
+                    <Col className="App-column1">Column 1</Col>
+                    <Col className="App-column2">Column 2</Col>
+                </Row>
+            </Container>
         </div>
     );
 }
