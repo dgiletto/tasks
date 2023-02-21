f0d32b9dd56574ab1bedfc1a15cdc51d8ffd1b73
Dylan Giletto <dgiletto@udel.edu>
2023-02-21 13:41:09 -0500

    fahrenheitToCelsius and add3 solved

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    temperature -= 32;
+    return temperature * (5 / 9);
 }
 
 /**
@@ -12,7 +13,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let total: number = 0;
+    if (first > 0) {
+        total += first;
+    }
+    if (second > 0) {
+        total += second;
+    }
+    if (third > 0) {
+        total += third;
+    }
+    return total;
 }
 
 /**
