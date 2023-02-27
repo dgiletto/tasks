91c0f463957bcd72e07489f5cbb390c2ca710140
Dylan Giletto <dgiletto@udel.edu>
2023-02-27 18:56:20 -0500

    Solved BookEndList, TripleNumbers, stringsToIntegers, removeDollars, shoutIfExclaiming, and countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,13 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (!numbers.length) {
+        return numbers;
+    } else if (numbers.length === 1) {
+        return [numbers[0], numbers[0]];
+    }
+    const newList: number[] = [numbers[0], numbers[numbers.length - 1]];
+    return newList;
 }
 
 /**
@@ -13,7 +19,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((num: number): number => num * 3);
 }
 
 /**
@@ -21,7 +27,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((num: string): number =>
+        Number.isNaN(+num) ? 0 : +num
+    );
 }
 
 /**
@@ -32,7 +40,10 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    amounts = amounts.map((num: string): string =>
+        num.indexOf("$") !== -1 ? num.replace("$", "") : num
+    );
+    return stringsToIntegers(amounts);
 };
 
 /**
@@ -41,7 +52,12 @@ export const removeDollars = (amounts: string[]): number[] => {
  * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
-    return [];
+    messages = messages.filter(
+        (message: string): boolean => message.indexOf("?") === -1
+    );
+    return messages.map((message: string): string =>
+        message.indexOf("!") !== -1 ? message.toUpperCase() : message
+    );
 };
 
 /**
@@ -49,7 +65,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    words = words.filter((word: string): boolean => word.length < 4);
+    return words.length;
 }
 
 /**
@@ -58,7 +75,14 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    }
+    colors = colors.filter(
+        (color: string): boolean =>
+            color !== "red" && color !== "blue" && color !== "green"
+    );
+    return colors.length === 0;
 }
 
 /**
@@ -69,7 +93,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0
+    );
+    const str = addends.join("+");
+    return sum.toString() + "=" + str;
 }
 
 /**
@@ -82,5 +114,24 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    const first_negative = values.findIndex(
+        (value: number): boolean => value < 0
+    );
+    if (first_negative === -1) {
+        const sum = values.reduce(
+            (currentTotal: number, num: number) => currentTotal + num,
+            0
+        );
+        values.push(sum);
+    } else {
+        const new_list = values.filter(
+            (value: number): boolean => values.indexOf(value) < first_negative
+        );
+        const sum = new_list.reduce(
+            (currentTotal: number, num: number) => currentTotal + num,
+            0
+        );
+        values.splice(first_negative, 0, sum);
+    }
+    return values;
 }
