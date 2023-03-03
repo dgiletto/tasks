716a72ddc27f06381ab688f34af3730ae27d18b7
Dylan Giletto <dgiletto@udel.edu>
2023-03-03 16:34:53 -0500

    Solved toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -56,7 +56,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const message = question.name.substring(0, 10);
+    return question.id.toString() + ": " + message;
 }
 
 /**
